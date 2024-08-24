da204ede8c0beaa4332e4b10f2b84549ed211a5f
michiko lange taguchi <51973693+michi1008@users.noreply.github.com>
2024-08-24 13:54:31 -0700

    change URL for forget password

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -218,7 +218,7 @@ export const forgetPassword = asyncHandler(async (req, res) => {
       subject: "Reset Password",
       html: `<h1>Reset Your Password</h1>
         <p>Click on the following link to reset your password:</p>
-        <a href="http://localhost:3000/reset-password/${token}">http://localhost:3000/reset-password/${token}</a>
+        <a href="https://favoritefaves.onrender.com/reset-password/${token}">https://favoritefaves.onrender.com/reset-password/${token}</a>
         <p>The link will expire in 10 minutes.</p>
         <p>If you didn't request a password reset, please ignore this email.</p>`,
     };
