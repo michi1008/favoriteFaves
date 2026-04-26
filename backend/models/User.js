5a064f0a4fe5f1b2bceedeb04c2e80508542f03d
Michiko Taguchi <michi@Michikos-MacBook-Air.local>
2026-04-25 18:00:24 -0700

    fix the bug

--- backend/models/User.js
+++ backend/models/User.js
@@ -30,7 +30,7 @@ const userSchema = mongoose.Schema(
 // Password hashing middleware
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
-    next();
+    return next();
   }
 
   const salt = await bcrypt.genSalt(10);
